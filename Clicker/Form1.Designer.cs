642aa5dac36f0540173996d2a0a5661e65db5623
BiroMarton <rezoner9@gmail.com>
2017-07-14 15:34:49 +0200

    Clicker core elements with win condition.

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Money = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tier1 = new System.Windows.Forms.Button();
+            this.tier2 = new System.Windows.Forms.Button();
+            this.tier3 = new System.Windows.Forms.Button();
+            this.tier4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.winThis = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Money:  ";
+            // 
+            // Money
+            // 
+            this.Money.AutoSize = true;
+            this.Money.Location = new System.Drawing.Point(65, 21);
+            this.Money.Name = "Money";
+            this.Money.Size = new System.Drawing.Size(0, 13);
+            this.Money.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Make money!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tier1
+            // 
+            this.tier1.Location = new System.Drawing.Point(27, 122);
+            this.tier1.Name = "tier1";
+            this.tier1.Size = new System.Drawing.Size(102, 23);
+            this.tier1.TabIndex = 3;
+            this.tier1.Text = "Tier 1 (50$)";
+            this.tier1.UseVisualStyleBackColor = true;
+            this.tier1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tier2
+            // 
+            this.tier2.Location = new System.Drawing.Point(27, 166);
+            this.tier2.Name = "tier2";
+            this.tier2.Size = new System.Drawing.Size(102, 23);
+            this.tier2.TabIndex = 5;
+            this.tier2.Text = "Tier 2 (250$)";
+            this.tier2.UseVisualStyleBackColor = true;
+            this.tier2.Click += new System.EventHandler(this.tier2_Click);
+            // 
+            // tier3
+            // 
+            this.tier3.Location = new System.Drawing.Point(27, 215);
+            this.tier3.Name = "tier3";
+            this.tier3.Size = new System.Drawing.Size(102, 23);
+            this.tier3.TabIndex = 6;
+            this.tier3.Text = "Tier 3(1000$)";
+            this.tier3.UseVisualStyleBackColor = true;
+            this.tier3.Click += new System.EventHandler(this.tier3_Click);
+            // 
+            // tier4
+            // 
+            this.tier4.Location = new System.Drawing.Point(27, 259);
+            this.tier4.Name = "tier4";
+            this.tier4.Size = new System.Drawing.Size(102, 23);
+            this.tier4.TabIndex = 7;
+            this.tier4.Text = "Tier 4(1750$)";
+            this.tier4.UseVisualStyleBackColor = true;
+            this.tier4.Click += new System.EventHandler(this.tier4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // winThis
+            // 
+            this.winThis.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.winThis.Location = new System.Drawing.Point(280, 165);
+            this.winThis.Name = "winThis";
+            this.winThis.Size = new System.Drawing.Size(192, 73);
+            this.winThis.TabIndex = 9;
+            this.winThis.Text = "Win this game by earning 5000$!";
+            this.winThis.UseVisualStyleBackColor = true;
+            this.winThis.Visible = false;
+            this.winThis.Click += new System.EventHandler(this.winThis_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Money per click: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 325);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.winThis);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tier4);
+            this.Controls.Add(this.tier3);
+            this.Controls.Add(this.tier2);
+            this.Controls.Add(this.tier1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Money);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Idiot Clicker 4000 Deluxe!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Money;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button tier1;
+        private System.Windows.Forms.Button tier2;
+        private System.Windows.Forms.Button tier3;
+        private System.Windows.Forms.Button tier4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button winThis;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+
+        public double money = 0;
+        public double amount = 1.5;
+        bool isUnlocked1 = false;
+        bool isUnlocked2 = false;
+        bool isUnlocked3 = false;
+        bool isUnlocked4 = false;
+        bool winnable = false;
+        
+
+
+        public  void UpdateCounter()
+        {
+            Money.Text = money + "$";
+            label2.Text = amount + "$";
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Money.Text = money.ToString();
+            label2.Text = amount + "$";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            money += amount;
+            UpdateCounter();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (money >= 50 && isUnlocked1 == false)
+            {
+                
+                money -= 50;
+                isUnlocked1 = true;
+                amount += 2;
+                UpdateCounter();
+                tier1.Visible = false;
+            }
+
+
+        }
+
+        private void tier2_Click(object sender, EventArgs e)
+        {
+            if (money >= 250 && isUnlocked2 == false)
+            {
+                
+                money -= 250;
+                isUnlocked2 = true;
+                amount += 5.5;
+                UpdateCounter();
+                tier2.Visible = false;
+            }
+        }
+
+        private void tier3_Click(object sender, EventArgs e)
+        {
+            if (money >= 150 && isUnlocked3 == false)
+            {
+                
+                money -= 1000;
+                isUnlocked3 = true;
+                amount += 10;
+                UpdateCounter();
+                tier3.Visible = false;
+            }
+        }
+
+        private void tier4_Click(object sender, EventArgs e)
+        {
+            if (money >= 1750 && isUnlocked4 == false)
+            {
+
+                money -= 1750;
+                isUnlocked4 = true;
+                amount += 35.5;
+                UpdateCounter();
+                tier4.Visible = false;
+                winnable = true;
+
+                if (winnable == true)
+                {
+                    winThis.Visible = true;
+                }
+            }
+        }
+
+        private void winThis_Click(object sender, EventArgs e)
+        {
+            if (winnable == true && money >= 5000)
+            {
+                MessageBox.Show("Congratulation on wasting your time!!!");
+                timer1.Enabled = true;
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                this.Close();
+            
+            
+            
+        }
+
+
+    }
+}
